4c9838d2b24a0b7c24747e9fed34f94a18cdfc01
Farhad <fgadirli@turanbank.az>
2022-11-23 17:35:24 +0400

    define Person Entity

--- src/main/java/com/example/database/databasedemo/entity/Person.java
+++ src/main/java/com/example/database/databasedemo/entity/Person.java
@@ -1,8 +1,14 @@
 package com.example.database.databasedemo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String location;
@@ -13,6 +19,12 @@ public class Person {
 
     public Person(int id, String name, String location, Date birthDate) {
         this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public Person(String name, String location, Date birthDate) {
         this.name = name;
         this.location = location;
         this.birthDate = birthDate;
