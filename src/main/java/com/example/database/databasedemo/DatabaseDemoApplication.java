4e5b8569ae30614093e20412357f982ee9a13b1d
Farhad <fgadirli@turanbank.az>
2022-11-23 15:54:45 +0400

    findAll()

--- src/main/java/com/example/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/example/database/databasedemo/DatabaseDemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.database.databasedemo;
 
+import com.example.database.databasedemo.jdbc.PersonJdbcDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+    private final PersonJdbcDAO dao;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public DatabaseDemoApplication(PersonJdbcDAO personJdbcDAO) {
+        this.dao = personJdbcDAO;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DatabaseDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All persons -> {} ", dao.findAll());
+    }
 }
--- src/main/java/com/example/database/databasedemo/jdbc/PersonJdbcDAO.java
+++ src/main/java/com/example/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -1,2 +1,23 @@
-package com.example.database.databasedemo.jdbc;public class PersonJdbcDAO {
+package com.example.database.databasedemo.jdbc;
+
+import com.example.database.databasedemo.entity.Person;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PersonJdbcDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //select * from person
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
 }
