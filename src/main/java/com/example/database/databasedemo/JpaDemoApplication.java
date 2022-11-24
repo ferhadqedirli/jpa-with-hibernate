863a65ce8d010b4c9227bdd382a8e4ad145dba28
Farhad <fgadirli@turanbank.az>
2022-11-24 11:14:52 +0400

    insert and update with jpa

--- src/main/java/com/example/database/databasedemo/JpaDemoApplication.java
+++ src/main/java/com/example/database/databasedemo/JpaDemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.database.databasedemo;
 
+import com.example.database.databasedemo.entity.Person;
 import com.example.database.databasedemo.jpa.PersonJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 
 @SpringBootApplication
@@ -24,6 +27,8 @@ public class JpaDemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("Person info -> {} ", repository.findById(10002));
+        logger.info("Inserting Person -> {} ", repository.insert(new Person("Tony", "Berlin", new Date())));
+        logger.info("Updating Person -> {} ", repository.update(new Person(1,"Tony", "Tokyo", new Date())));
+        logger.info("Person info -> {} ", repository.findById(1));
     }
 }
